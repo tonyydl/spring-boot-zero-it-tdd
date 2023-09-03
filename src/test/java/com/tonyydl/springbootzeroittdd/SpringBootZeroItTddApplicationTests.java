e3a200533c0e5112261dfd4e81b5192b9ef5fbec
Tony Yang <superzoro168@gmail.com>
2023-09-03 12:46:07 +0800

    Initial project

---
+++ src/test/java/com/tonyydl/springbootzeroittdd/SpringBootZeroItTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyydl.springbootzeroittdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZeroItTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
